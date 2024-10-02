330b8c9eabcd52242a0086e9b052066afe662265
HewhosaysIam <yijiahe2021@hotmail.com>
2024-10-02 20:49:27 +0800

    no audio

--- js/fcp-instructions.js
+++ js/fcp-instructions.js
@@ -45,83 +45,60 @@ preload_images = [
   "img/replay.png",
 ];
 
-preload_audio = [
-  "audio/instructions1.wav",
-  "audio/instructions2.wav",
-  "audio/instructions3.wav",
-  "audio/instructions4.wav",
-  "audio/instructions5.wav",
-  "audio/instructions6.wav",
-  "audio/instructions7.wav",
-  "audio/instructions8.wav",
-  "audio/instructions9.wav",
-  "audio/instructions10.wav",
-  "audio/instructions11.wav",
-  "audio/instructions12.wav",
-  "audio/instructions13.wav",
-  "audio/instructions14.wav",
-  "audio/instructions15.wav",
-  "audio/instructions16.wav",
-  "audio/instructions17.wav",
-  "audio/instructions18.wav",
-  "audio/instructions19.wav",
-  "audio/beep_loop.wav",
-  "audio/turtle.wav",
-  "audio/shark.wav",
-];
+// Removed preload_audio array since we're not using audio.
+// preload_audio = [
+//   "audio/instructions1.wav",
+//   "audio/instructions2.wav",
+//   // ... more audio files
+// ];
 
 //------------------------------------//
-// Define block #1
+// Define Instructions Block #1
 //------------------------------------//
 
-// Instructions block #1.
+// Updated instruction trials to use 'instructions' plugin and removed audio stimuli.
 var instructions01a = {
-  type: 'audio-instructions',
-  prompt: [
+  type: 'instructions',
+  pages: [
     `<img src="img/instructions/instructions1.png" style="${style}"></img>`,
   ],
- // stimulus: ['audio/instructions1.wav'],
-  choices: ['Next'],
-}
+  show_clickable_nav: true,
+};
 
 var instructions01b = {
-  type: 'audio-instructions',
-  prompt: [
+  type: 'instructions',
+  pages: [
     `<img src="img/instructions/instructions2.png" style="${style}"></img>`,
   ],
-//  stimulus: ['audio/instructions2.wav'],
-  choices: ['Next'],
-}
+  show_clickable_nav: true,
+};
 
 var instructions01c = {
-  type: 'audio-instructions',
-  prompt: [
+  type: 'instructions',
+  pages: [
     `<img src="img/instructions/instructions3.png" style="${style}"></img>`,
   ],
-//  stimulus: ['audio/instructions3.wav'],
-  choices: ['Next'],
-}
+  show_clickable_nav: true,
+};
 
 var instructions01d = {
-  type: 'audio-instructions',
-  prompt: [
+  type: 'instructions',
+  pages: [
     `<img src="img/instructions/instructions4.png" style="${style}"></img>`,
   ],
-  stimulus: ['audio/instructions4.wav'],
-  choices: ['Next'],
-}
+  show_clickable_nav: true,
+};
 
 var instructions01e = {
-  type: 'audio-instructions',
-  prompt: [
+  type: 'instructions',
+  pages: [
     `<img src="img/instructions/instructions5.png" style="${style}"></img>`,
   ],
-  stimulus: ['audio/instructions5.wav'],
-  choices: ['Next'],
-}
-
+  show_clickable_nav: true,
+};
 
 // Practice trials #1.
+// Assuming 'fcp-practice' doesn't require audio changes.
 var practice01 = {
   type: 'fcp-practice',
   arcade_colors: ['#EDC948', '#46d3c8'],
@@ -134,68 +111,62 @@ var practice01 = {
     { arcade_outcomes: [10, 0] },
     { arcade_outcomes: [10, 0] },
   ],
-  data: { phase: 'practice' }
-}
+  data: { phase: 'practice' },
+};
 
 var instructions02a = {
-  type: 'audio-instructions',
-  prompt: [
+  type: 'instructions',
+  pages: [
     `<img src="img/instructions/instructions6.png" style="${style}"></img>`,
   ],
-  stimulus: ['audio/instructions6.wav'],
-  choices: ['Next'],
-}
+  show_clickable_nav: true,
+};
 
 var instructions02b = {
-  type: 'audio-instructions',
-  prompt: [
+  type: 'instructions',
+  pages: [
     `<img src="img/instructions/instructions7.png" style="${style}"></img>`,
   ],
-  stimulus: ['audio/instructions7.wav'],
-  choices: ['Next'],
-}
+  show_clickable_nav: true,
+};
 
 var instructions02c = {
-  type: 'audio-instructions',
-  prompt: [
+  type: 'instructions',
+  pages: [
     `<img src="img/instructions/instructions8.png" style="${style}"></img>`,
   ],
-  stimulus: ['audio/instructions8.wav'],
-  choices: ['Next'],
-}
+  show_clickable_nav: true,
+};
 
 // Quiz #1.
 var quiz01 = {
   type: 'fcp-comprehension',
   prompts: [
-    "<b><i>True</i> or <i>False</i>:</b>&nbsp;&nbsp;Winning or losing at a slot machine depends only on how lucky that machine is.",
-    "<b><i>True</i> or <i>False</i>:</b>&nbsp;&nbsp;Some slot machines may be luckier than others.",
+    "<b><i>True</i> or <i>False</i>:</b> Winning or losing at a slot machine depends only on how lucky that machine is.",
+    "<b><i>True</i> or <i>False</i>:</b> Some slot machines may be luckier than others.",
   ],
-  correct: ["true", "true"]
-}
+  correct: ["true", "true"],
+};
 
 //------------------------------------//
-// Define instructions block 2
+// Define Instructions Block #2
 //------------------------------------//
 
-// Instructions block #2.
 var instructions03a = {
-  type: 'audio-instructions',
-  prompt: [
+  type: 'instructions',
+  pages: [
     `<img src="img/instructions/instructions9.png" style="${style}"></img>`,
   ],
-  stimulus: ['audio/instructions9.wav'],
-  choices: ['Next'],
-}
+  show_clickable_nav: true,
+};
 
 var instructions03b = {
-  type: 'audio-instructions',
-  prompt: [
+  type: 'instructions',
+  pages: [
     `<img src="img/instructions/instructions10.png" style="${style}"></img>`,
   ],
-  stimulus: ['audio/instructions10.wav'],
-  choices: ['Next'],
-}
+  show_clickable_nav: true,
+};
 
 // Practice trials #2.
 var practice02 = {
@@ -205,18 +176,17 @@ var practice02 = {
   bonus_offer: 0,
   correct: 1,
   practice_key: 38,
-  data: { phase: 'practice' }
-}
+  data: { phase: 'practice' },
+};
 
-// Instructions block #3.
+// Instructions Block #3.
 var instructions04a = {
-  type: 'audio-instructions',
-  prompt: [
+  type: 'instructions',
+  pages: [
     `<img src="img/instructions/instructions11.png" style="${style}"></img>`,
   ],
-  stimulus: ['audio/instructions11.wav'],
-  choices: ['Next'],
-}
+  show_clickable_nav: true,
+};
 
 // Practice trials #3.
 var practice03 = {
@@ -226,119 +196,111 @@ var practice03 = {
   bonus_offer: 6,
   correct: 1,
   practice_key: 40,
-  data: { phase: 'practice' }
-}
+  data: { phase: 'practice' },
+};
 
 var instructions05a = {
-  type: 'audio-instructions',
-  prompt: [
+  type: 'instructions',
+  pages: [
     `<img src="img/instructions/instructions12.png" style="${style}"></img>`,
   ],
-  stimulus: ['audio/instructions12.wav'],
-  choices: ['Next'],
-}
+  show_clickable_nav: true,
+};
 
 var instructions05b = {
-  type: 'audio-instructions',
-  prompt: [
+  type: 'instructions',
+  pages: [
     `<img src="img/instructions/instructions13.png" style="${style}"></img>`,
   ],
-  stimulus: ['audio/instructions13.wav'],
-  choices: ['Next'],
-}
-
+  show_clickable_nav: true,
+};
 
 var instructions05c = {
-  type: 'audio-instructions',
-  prompt: [
+  type: 'instructions',
+  pages: [
     `<img src="img/instructions/instructions14.png" style="${style}"></img>`,
   ],
-  stimulus: ['audio/instructions14.wav'],
-  choices: ['Next'],
-}
+  show_clickable_nav: true,
+};
 
+//------------------------------------//
+// Define Instructions Block #4
+//------------------------------------//
 
-// Instructions block #4.
 var instructions06a = {
-  type: 'audio-instructions',
-  prompt: [
+  type: 'instructions',
+  pages: [
     `<img src="img/instructions/instructions15.png" style="${style}"></img>`,
   ],
-  stimulus: ['audio/instructions15.wav'],
-  choices: ['Next'],
-}
+  show_clickable_nav: true,
+};
 
 var instructions06b = {
-  type: 'audio-instructions',
-  prompt: [
+  type: 'instructions',
+  pages: [
     `<img src="img/instructions/instructions16.png" style="${style}"></img>`,
   ],
-  stimulus: ['audio/instructions16.wav'],
-  choices: ['Next'],
-}
+  show_clickable_nav: true,
+};
 
 var instructions06c = {
-  type: 'audio-instructions',
-  prompt: [
+  type: 'instructions',
+  pages: [
     `<img src="img/instructions/instructions17.png" style="${style}"></img>`,
   ],
-  stimulus: ['audio/instructions17.wav'],
-  choices: ['Next'],
-}
+  show_clickable_nav: true,
+};
 
 var instructions06d = {
-  type: 'audio-instructions',
-  prompt: [
+  type: 'instructions',
+  pages: [
     `<img src="img/instructions/instructions18.png" style="${style}"></img>`,
   ],
-  stimulus: ['audio/instructions18.wav'],
-  choices: ['Next'],
-}
+  show_clickable_nav: true,
+};
 
 var instructions06e = {
-  type: 'audio-instructions',
-  prompt: [
+  type: 'instructions',
+  pages: [
     `<img src="img/instructions/instructions19.png" style="${style}"></img>`,
   ],
-  stimulus: ['audio/instructions19.wav'],
-  choices: ['Next'],
-}
-
+  show_clickable_nav: true,
+};
 
 // Quiz #2.
 var quiz02 = {
   type: 'fcp-comprehension',
   prompts: [
-    "<b><i>True</i> or <i>False</i>:</b>&nbsp;&nbsp;How lucky a slot machine is changes over time.",
-    "<b><i>True</i> or <i>False</i>:</b>&nbsp;&nbsp;How lucky a slot machine is changes if it is on the left or the right side of the screen.",
-    "<b><i>True</i> or <i>False</i>:</b>&nbsp;&nbsp;How lucky a slot machine is depends on whether you or the computer chooses it.",
-    "<b><i>True</i> or <i>False</i>:</b>&nbsp;&nbsp;The tokens I earn will affect my bonus payment.",
+    "<b><i>True</i> or <i>False</i>:</b> How lucky a slot machine is changes over time.",
+    "<b><i>True</i> or <i>False</i>:</b> How lucky a slot machine is changes if it is on the left or the right side of the screen.",
+    "<b><i>True</i> or <i>False</i>:</b> How lucky a slot machine is depends on whether you or the computer chooses it.",
+    "<b><i>True</i> or <i>False</i>:</b> The tokens I earn will affect my bonus payment.",
   ],
-  correct: ["true", "false", "false", "true"]
-}
-
-// Explicit instructions
-var explicit_instructions1 = {
-  type: 'audio-instructions',
-  prompt: [
-    `<img src="img/instructions/explicit1.png" style="${style}"></img>`,
-  ],
-  stimulus: ['audio/explicit1.wav'],
-  choices: ['Next'],
-}
-
-var explicit_instructions2 = {
-  type: 'audio-instructions',
-  prompt: [
-    `<img src="img/instructions/explicit2.png" style="${style}"></img>`,
-  ],
-  stimulus: ['audio/explicit2.wav'],
-  choices: ['Next'],
-}
-
+  correct: ["true", "false", "false", "true"],
+};
 
 //------------------------------------//
-// Define instructions blocks.
+// Define Explicit Instructions
+//------------------------------------//
+
+var explicit_instructions1 = {
+  type: 'instructions',
+  pages: [
+    `<img src="img/instructions/explicit1.png" style="${style}"></img>`,
+  ],
+  show_clickable_nav: true,
+};
+
+var explicit_instructions2 = {
+  type: 'instructions',
+  pages: [
+    `<img src="img/instructions/explicit2.png" style="${style}"></img>`,
+  ],
+  show_clickable_nav: true,
+};
+
+//------------------------------------//
+// Define Instruction Loops
 //------------------------------------//
 
 var instructions_loop_01 = {
@@ -355,20 +317,13 @@ var instructions_loop_01 = {
     quiz01,
   ],
   loop_function: function (data) {
-
     // Extract number of errors.
     const num_errors = data.values().slice(-1)[0].num_errors;
 
     // Check if instructions should repeat.
-    if (num_errors > max_errors) {
-      return true;
-    } else {
-      return false;
-    }
-
-  }
-
-}
+    return num_errors > max_errors;
+  },
+};
 
 var instructions_loop_02 = {
   timeline: [
@@ -380,8 +335,8 @@ var instructions_loop_02 = {
     instructions05a,
     instructions05b,
     instructions05c,
-  ]
-}
+  ],
+};
 
 var instructions_loop_03 = {
   timeline: [
@@ -393,25 +348,14 @@ var instructions_loop_03 = {
     quiz02,
   ],
   loop_function: function (data) {
-
     // Extract number of errors.
     const num_errors = data.values().slice(-1)[0].num_errors;
 
     // Check if instructions should repeat.
-    if (num_errors > max_errors) {
-      return true;
-    } else {
-      return false;
-    }
-
-  }
-
-}
+    return num_errors > max_errors;
+  },
+};
 
 var explicit_instructions = {
-  timeline: [
-    explicit_instructions1,
-    explicit_instructions2,
-  ]
-
-}
+  timeline: [explicit_instructions1, explicit_instructions2],
+};
